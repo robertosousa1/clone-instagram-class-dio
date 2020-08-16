987dae850eb6f4738d1e92b4448e77178b5f22fa
Roberto <rm77831@fiap.com.br>
2020-08-16 16:38:10 -0300

    Estiliza página Feed

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -1,24 +1,73 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { FlatList } from 'react-native';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5F5F5',
-  },
-  title: {
-    fontSize: 24,
-    textAlign: 'center',
-    margin: 10,
-  },
-});
+import api from '../../services/api';
 
-export default function App() {
+import {
+  Container,
+  Post,
+  Header,
+  Avatar,
+  Name,
+  Description,
+  Loading,
+} from './styles';
+
+export default function Feed() {
+  const [feed, setFeed] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [refreshing, setRefreshing] = useState(false);
+
+  async function loadPage() {
+    if (loading) return;
+
+    setLoading(true);
+
+    const { data } = await api.get(`/feed?_expand=author`);
+
+    setLoading(false);
+
+    setFeed(data);
+  }
+
+  async function refreshList() {
+    setRefreshing(true);
+
+    await loadPage();
+
+    setRefreshing(false);
+  }
+
+  useEffect(() => {
+    loadPage();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello, DIO!</Text>
-    </View>
+    <Container>
+      <FlatList
+        key="list"
+        data={feed}
+        keyExtractor={(item) => String(item.id)}
+        viewabilityConfig={{
+          viewAreaCoveragePercentThreshold: 10,
+        }}
+        showsVerticalScrollIndicator={false}
+        onRefresh={refreshList}
+        refreshing={refreshing}
+        ListFooterComponent={loading && <Loading />}
+        renderItem={({ item }) => (
+          <Post>
+            <Header>
+              <Avatar source={{ uri: item.author.avatar }} />
+              <Name>{item.author.name}</Name>
+            </Header>
+
+            <Description>
+              <Name>{item.author.name}</Name> {item.description}
+            </Description>
+          </Post>
+        )}
+      />
+    </Container>
   );
 }
