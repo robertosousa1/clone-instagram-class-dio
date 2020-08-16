56ad80a2840ff749a7bd29b485ea5a74d9b47445
Roberto <rm77831@fiap.com.br>
2020-08-16 15:22:55 -0300

    Hello, DIO

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5F5F5',
+  },
+  title: {
+    fontSize: 24,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello, DIO!</Text>
+    </View>
+  );
+}
